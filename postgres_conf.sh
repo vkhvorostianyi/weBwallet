afb13160cf5de7998582c60fe883ae43cd90ac47
Viacheslav Khvorostianyi <slavafh@gmail.com>
2019-01-22 16:49:26 +0200

    Update postgres_conf.sh

--- postgres_conf.sh
+++ postgres_conf.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 sudo su postgres
 psql
-create database $1;
-create user $2 with password $3;
-ALTER ROLE $2 SET client_encoding TO 'utf8';
-ALTER ROLE $2 SET default_transaction_isolation TO 'read committed';
-ALTER ROLE $2 SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE $1 TO $2;
+create database testdb;
+create user test_user with password 'test_psw';
+ALTER ROLE test_user SET client_encoding TO 'utf8';
+ALTER ROLE test_user SET default_transaction_isolation TO 'read committed';
+ALTER ROLE test_user SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE testdb TO test_user;
